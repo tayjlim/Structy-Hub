68b5d54ce2748a8212a490fe09cd83e07d778b1d
Taylor Lim <120624931+tayjlim@users.noreply.github.com>
2024-01-08 12:36:03 -0800

    Solved 'connected components count' (.py)

--- connected_components_count/connected_components_count.py
+++ connected_components_count/connected_components_count.py
@@ -2,8 +2,9 @@ def connected_components_count(graph):
   count = 0
   visited = set()
   for node in graph:
-    print(node)
+    print('this is the starting',node)
     if explore(graph,node,visited) == True:
+      print('increment count',node)
       count +=1
   return count
 ​
@@ -14,9 +15,17 @@ def explore (graph, node, visited):
   
   visited.add(node)
   for neighbor in graph[node]:
+    print('neighbor',neighbor)
     explore(graph,neighbor,visited)
   
   return True
   
-  
-  
+connected_components_count({
+  0: [8, 1, 5],
+  1: [0],
+  5: [0, 8],
+  8: [0, 5],
+  2: [3, 4],
+  3: [2, 4],
+  4: [3, 2]
+}) # -> 2
